631463c588090241bbc3905653708fc0f8395abb
NikKowPHP <nik.kow@outlook.com>
2025-02-05 11:44:26 +0100

    structurize the our process section

--- src/components/sections/our-process/our-process.tsx
+++ src/components/sections/our-process/our-process.tsx
@@ -3,7 +3,7 @@ import { HeroButtons } from '../hero/hero-buttons'
 import { getTranslations } from 'next-intl/server'
 
 export const OurProcess = async () => {
-  const t = await getTranslations('hero')
+  const t = await getTranslations('processSection')
 
   return (
     <Suspense
@@ -13,24 +13,46 @@ export const OurProcess = async () => {
         </div>
       }
     >
-      <section className="flex flex-col items-center text-center pt-[180px] sm:pt-[180px] lg:pt-[160px] pb-[60px] sm:pb-[80px] lg:pb-[100px] space-y-6 sm:space-y-8"
+      <section
+        className=""
         itemScope
         itemType="https://schema.org/WebPageElement"
       >
-        <h1 className="text-[40px] sm:text-[56px] lg:text-[80px] leading-[1.1] font-medium tracking-[-0.02em] text-primary"
-          itemProp="headline"
-        >
-          {t('title1')}
-          <br className="hidden sm:block" />
-          <span className="sm:hidden"> </span>
-          {t('title2')}
-        </h1>
-        <p className="text-[18px] sm:text-[20px] lg:text-[24px] text-gray-900 leading-relaxed max-w-xl sm:max-w-2xl px-4 sm:px-0">
-          {t('subtitle')}
-        </p>
-        <HeroButtons />
+        <ProcessTitleSubtitle t={t} />
+        <ProcessItemList t={t} />
       </section>
     </Suspense>
-
   )
+}
+const ProcessTitleSubtitle = ( t: any ) => {
+  return (
+    <>
+      <h1
+        className="text-[40px] sm:text-[56px] lg:text-[48px] leading-[1.1] font-medium tracking-[-0.02em] text-primary"
+        itemProp="headline"
+      >
+        {t('title')}
+        <br className="hidden sm:block" />
+        <h3 className="text-[18px] sm:text-[20px] lg:text-[24px] text-gray-600 leading-relaxed max-w-xl sm:max-w-2xl px-4 sm:px-0">
+          {t('subtitle')}
+        </h3>
+      </h1>
+    </>
+  )
+}
+
+const ProcessItem = () => {
+  return (
+    <div>
+      {/* <h2>{t('title')}</h2> */}
+      <div>
+        <ul>
+          <li>{/* <p>{t('list.item1')}</p> */}</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+const ProcessItemList = ( t: any ) => {
+  return <div></div>
 }
