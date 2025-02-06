d370653ab3c0fd790ddab850e9103788b37e8281
NikKowPHP <nik.kow@outlook.com>
2025-02-06 11:59:13 +0100

    feat: implement youtube video in float

--- src/components/sections/float-video/float-video.tsx
+++ src/components/sections/float-video/float-video.tsx
@@ -1,34 +1,41 @@
 import { Suspense } from 'react'
-import { getTranslations } from 'next-intl/server'
+import YouTube from 'react-youtube'
 
 export const FloatVideo = async () => {
-  const t = await getTranslations('hero')
+  const videoId = 'vKb9WcH8Qpg'
+
+  const opts = {
+    height: '150',
+    width: '200',
+    playerVars: {
+      autoplay: 1,
+      mute: 1,
+      controls: 0,
+      loop: 1,
+      playlist: videoId, 
+    },
+  }
 
   return (
     <Suspense
       fallback={
-        <div className="flex items-center justify-center min-h-[200px]">
+        <div className="fixed bottom-[50px] right-5 min-h-[200px]">
           Loading...
         </div>
       }
     >
-      <section className="flex flex-col items-center text-center pt-[180px] sm:pt-[180px] lg:pt-[160px] pb-[60px] sm:pb-[80px] lg:pb-[100px] space-y-6 sm:space-y-8"
+      <section className="fixed bottom-[65px] right-[10px]"
         itemScope
         itemType="https://schema.org/WebPageElement"
       >
-        <h1 className="text-[40px] sm:text-[56px] lg:text-[80px] leading-[1.1] font-medium tracking-[-0.02em] text-primary"
-          itemProp="headline"
-        >
-          {t('title1')}
-          <br className="hidden sm:block" />
-          <span className="sm:hidden"> </span>
-          {t('title2')}
-        </h1>
-        <p className="text-[18px] sm:text-[20px] lg:text-[24px] text-gray-900 leading-relaxed max-w-xl sm:max-w-2xl px-4 sm:px-0">
-          {t('subtitle')}
-        </p>
+        <div className="w-[200px] h-[150px] rounded-lg overflow-hidden">
+          <YouTube
+            videoId={videoId}
+            opts={opts}
+            className="w-full h-full"
+          />
+        </div>
       </section>
     </Suspense>
-
   )
 }
