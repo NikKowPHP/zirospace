d009091c7569edab8540796e5782b5fd40c2dc80
NikKowPHP <nik.kow@outlook.com>
2025-02-06 08:23:30 +0100

    feat: Finish the our processes section

--- src/components/sections/our-process/our-process.tsx
+++ src/components/sections/our-process/our-process.tsx
@@ -8,13 +8,13 @@ export const OurProcess = async () => {
   return (
     <Suspense
       fallback={
-        <div className="flex items-center justify-center min-h-[200px]">
+        <div className="flex items-center justify-center min-h-[200px] ">
           Loading...
         </div>
       }
     >
       <section
-        className="border border-gray-700"
+        className="border border-gray-700 "
         itemScope
         itemType="https://schema.org/WebPageElement"
       >
@@ -26,25 +26,25 @@ export const OurProcess = async () => {
 }
 const ProcessTitleSubtitle = ({ t }: { t: any }) => {
   return (
-    <>
+    <div className='mb-[36px] flex flex-col items-center justify-center '>
       <h1
-        className="text-[40px] sm:text-[56px] lg:text-[48px] leading-[1.1] font-medium tracking-[-0.02em] text-primary"
+        className="text-[40px] sm:text-[56px] lg:text-[48px] leading-[1.1] font-medium tracking-[-0.02em] text-primary mb-[16px]"
         itemProp="headline"
       >
         {t('title')}
        
       </h1>
-      <h3 className="text-[18px] sm:text-[20px] lg:text-[24px] text-gray-600 leading-relaxed max-w-xl sm:max-w-2xl px-4 sm:px-0">
+      <h3 className="text-[14px] sm:text-[16px] lg:text-[18px] text-gray-600 leading-relaxed max-w-xl sm:max-w-2xl px-4 sm:px-0">
           {t('subtitle')}
         </h3>
-    </>
+    </div>
   )
 }
 
 const ProcessItem = ({ t, index, item }: { t: any; index: number; item: any }) => {
   return (
-    <div key={index} className="p-4 border rounded shadow-sm flex flex-col gap-[16px] ">
-      <h2>0{index + 1}</h2>
+    <div key={index} className="p-[36px] rounded-xl border border-red-500 bg-gray-100 shadow-sm flex flex-col gap-[16px] ">
+      <h4 className='text-[16px] leading-[1.2]'>0{index + 1}</h4>
       <h2 className="text-lg text-black">{t(`${item.title}`)}</h2>
       <p className=" text-gray-600">
         <ul className="flex flex-col gap-[16px]">
@@ -67,7 +67,7 @@ const ProcessItemList = async ({
   const items = await processItems
   console.log(items)
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 sm:gap-[20px]">
+    <div className="grid grid-cols-1 sm:grid-cols-2 sm:gap-[20px] max-w-3xl justify-center mx-auto">
       {items.map((item: any, index: number) => (
         <ProcessItem t={t} index={index} item={item} key={index} />
       ))}
