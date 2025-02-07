32f9eff43e5247a0eee395ee102d6c6cfea59f54
NikKowPHP <nik.kow@outlook.com>
2025-02-07 11:24:24 +0100

    create dto

---
+++ src/infrastructure/dto/testimonial.dto.ts
@@ -0,0 +1,11 @@
+export interface TestimonialDTO {
+  id: string;
+  author: string;
+  role: string;
+  company: string;
+  quote: string;
+  image: string;
+  image_alt: string;
+  created_at: string;
+  updated_at: string;
+}
