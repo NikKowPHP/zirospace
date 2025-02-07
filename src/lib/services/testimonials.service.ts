f86e445771a61e9c67119c1f6acfd539f6aa71bb
NikKowPHP <nik.kow@outlook.com>
2025-02-07 11:26:59 +0100

    create service for testimonials

---
+++ src/lib/services/testimonials.service.ts
@@ -0,0 +1,39 @@
+// ... existing code ...
+import { ITestimonialRepository } from "../interfaces/testimonials.interface"
+import { Testimonial } from "@/domain/models/testimonial.model"
+import { testimonialRepositoryLocal } from "../repositories/testimonials.local.repository"
+import { Locale } from "@/i18n"
+
+export class TestimonialService {
+  private testimonialRepository: ITestimonialRepository
+  constructor() {
+    if(process.env.MOCK_REPOSITORIES === 'true') {
+      this.testimonialRepository = testimonialRepositoryLocal
+    } else {
+      this.testimonialRepository = testimonialRepositoryLocal // TODO: implement postgres repo
+    }
+  }
+
+  getTestimonials = async (locale: Locale): Promise<Testimonial[]> => {
+    return this.testimonialRepository.getTestimonials(locale)
+  }
+
+  getTestimonialById = async (id: string, locale: Locale): Promise<Testimonial | null> => {
+    return this.testimonialRepository.getTestimonialById(id, locale)
+  }
+
+  createTestimonial = async (testimonial: Omit<Testimonial, 'id'>, locale: Locale): Promise<Testimonial> => {
+    return this.testimonialRepository.createTestimonial(testimonial, locale)
+  }
+
+  updateTestimonial = async (id: string, testimonial: Partial<Testimonial>, locale: Locale): Promise<Testimonial | null> => {
+    return this.testimonialRepository.updateTestimonial(id, testimonial, locale)
+  }
+
+  deleteTestimonial = async (id: string, locale: Locale): Promise<boolean> => {
+    return this.testimonialRepository.deleteTestimonial(id, locale)
+  }
+}
+
+// export singleton
+export const testimonialService = new TestimonialService()
