8e5406b7dc28301a40f8ee9acbc89497739e5bb3
NikKowPHP <nik.kow@outlook.com>
2025-02-10 14:20:31 +0100

    display blog posts

--- src/app/[locale]/blog/page.tsx
+++ src/app/[locale]/blog/page.tsx
@@ -1,44 +1,51 @@
 import { type Locale } from '@/i18n'
 import Link from 'next/link'
 import Image from 'next/image'
-import { useTranslations } from 'next-intl'
-import { blogPosts } from '@/lib/data/blog-posts.mock.data'
+import { getTranslations } from 'next-intl/server'
+import { getBlogPostService } from '@/lib/services/blog-post.service'
+import { BlogPost } from '@/domain/models/blog-post.model'
+
 interface PageProps {
   params: {
     locale: Locale
   }
 }
 
+const blogPostService = await getBlogPostService();
 
-export default function BlogPage({ params }: PageProps) {
-  const { locale } = params
-  const t = useTranslations('blog')
+export default async function BlogPage({ params }: PageProps) {
+  const { locale } = await params;
+  
+  const t = await getTranslations('blog');
+  
+  const blogPosts = await blogPostService.getBlogPosts(locale);
+  console.log('blogPosts', blogPosts);
   
   return (
-    <div className=" mx-auto py-8 border border-red-500 py-[100px]">
-      <h1 className="text-[28px] font-bold  text-center mb-[64px]">{t('latest-articles')}</h1>
+    <div className="mx-auto py-8 border border-red-500 py-[100px]">
+      <h1 className="text-[28px] font-bold text-center mb-[64px]">{t('latest-articles')}</h1>
       <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[32px]">
         {blogPosts.map((post) => (
-          <BlogPost post={post} locale={locale} />
+          <BlogPostItem key={post.slug} post={post} locale={locale} />
         ))}
       </ul>
     </div>
   )
 }
 
-const BlogPost = ({
+const BlogPostItem = ({
   post,
   locale,
 }: {
-  post: (typeof blogPosts)[number]
+  post: BlogPost
   locale: Locale
 }) => {
-  return <li key={post.slug} className="border border-blue-500 rounded-xl ">
+  return <li  className="border border-blue-500 rounded-xl ">
     <Link href={`/${locale}/blog/${post.slug}`} className="flex flex-col gap-[8px] relative">
-      <Image className='w-full h-auto rounded-xl' src={post.imageurl} alt={post.imageAlt!} width={350} height={300} />
+      {post.imageurl && <Image className='w-full h-auto rounded-xl' src={post.imageurl} alt={post.imageAlt!} width={350} height={300} />}
       <h2 className="text-xl font-semibold border border-red-500 text-center text-[22px]">{post.title}</h2>
       <div className='absolute top-8  left-8 px-[10px] py-[5px] bg-white rounded-full'>
-        {post.createdAt}
+        {new Date(post.createdAt).toLocaleDateString()}
 
       </div>
     </Link>
