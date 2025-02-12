bc7a10c235b2f5d6e21a2191dc0f6ee52d3af7ba
NikKowPHP <nik.kow@outlook.com>
2025-02-12 11:26:41 +0100

    add bg white to the page wrapper

--- src/app/[locale]/client-wrapper.tsx
+++ src/app/[locale]/client-wrapper.tsx
@@ -6,12 +6,15 @@ import { LanguageSwitcher } from '@/components/ui/language-switcher/language-swi
 
 export function ClientWrapper({ children }: { children: React.ReactNode }) {
   return (
+      <div className='bg-white w-full'>
     <div className='bg-white  pb-[40px] container px-[20px] sm:px-[20px] relative'>
+
       <Navbar />
       {children}
         <Footer />
       <LanguageSwitcher />
     </div>
+      </div>
   )
 }
 
