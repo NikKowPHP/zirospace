61691b8262af166210eddb06f93bba72330f1e44
NikKowPHP <nik.kow@outlook.com>
2025-02-12 11:25:02 +0100

    remove lines on blog page

--- src/app/[locale]/blog/[slug]/page.tsx
+++ src/app/[locale]/blog/[slug]/page.tsx
@@ -24,7 +24,7 @@ export default async function BlogPostPage({ params }: PageProps) {
   }
 
   return (
-    <article className="blog-post py-8 py-[100px] max-w-5xl mx-auto border border-red-500 flex flex-col gap-[35px]">
+    <article className="blog-post py-8 py-[100px] max-w-5xl mx-auto  flex flex-col gap-[35px]">
       <header className="flex flex-col gap-8">
         <h1 className="text-3xl font-bold mb-4 text-center">{post.title}</h1>
         <Image
