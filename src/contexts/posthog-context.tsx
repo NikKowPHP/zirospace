ea94182717d4a7ce6fbf1294b02c7463d27113e1
NikKowPHP <nik.kow@outlook.com>
2025-02-22 16:41:14 +0100

    capture page leave

--- src/contexts/posthog-context.tsx
+++ src/contexts/posthog-context.tsx
@@ -19,6 +19,7 @@ export function PostHogProvider({ children }: { children: React.ReactNode }) {
       api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
       person_profiles: 'always', // or 'always' to create profiles for anonymous users as well
       capture_pageview: false, // Disable automatic pageview capture, as we capture manually
+      capture_pageleave: true
     })
   }, [])
 
