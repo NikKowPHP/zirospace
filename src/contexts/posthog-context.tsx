38c7d6c88a1a82e943c8a248f9bf73f71f6be381
NikKowPHP <nik.kow@outlook.com>
2025-02-22 15:07:17 +0100

    create posthog page view

--- src/contexts/posthog-context.tsx
+++ src/contexts/posthog-context.tsx
@@ -3,28 +3,29 @@
 import posthog from 'posthog-js'
 import { PostHogProvider as PHProvider } from 'posthog-js/react'
 import { useEffect } from 'react'
+import SuspendedPostHogPageView from '@/components/PostHogPageView'
 
-export function PostHogProvider({ children }) {
-    useEffect(() => {
-        if (!process.env.NEXT_PUBLIC_POSTHOG_KEY) {
-            console.log('PostHog key is not set')
-            return
-        }
-        if (!process.env.NEXT_PUBLIC_POSTHOG_HOST) {
-            console.log('PostHog host is not set')
-            return
-        }
-        posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
-            api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-            person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
-            capture_pageview: false // Disable automatic pageview capture, as we capture manually
-        })
-    }, [])
+export function PostHogProvider({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    if (!process.env.NEXT_PUBLIC_POSTHOG_KEY) {
+      console.log('PostHog key is not set')
+      return
+    }
+    if (!process.env.NEXT_PUBLIC_POSTHOG_HOST) {
+      console.log('PostHog host is not set')
+      return
+    }
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+      person_profiles: 'always', // or 'always' to create profiles for anonymous users as well
+      capture_pageview: false, // Disable automatic pageview capture, as we capture manually
+    })
+  }, [])
 
-return (
+  return (
     <PHProvider client={posthog}>
-        <PostHogPageView />
-        {children}
+      <SuspendedPostHogPageView />
+      {children}
     </PHProvider>
-)
+  )
 }
