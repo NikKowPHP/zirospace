db5a3adb2c6a4e00ec61df8bc90d3219de73cb50
NikKowPHP <nik.kow@outlook.com>
2025-02-24 11:44:16 +0100

    update modal

--- src/components/ui/modal/modal.tsx
+++ src/components/ui/modal/modal.tsx
@@ -34,7 +34,7 @@ const DialogContent = React.forwardRef<
     <DialogPrimitive.Content
       ref={ref}
       className={cn(
-        "fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-top-[22%] data-[state=open]:slide-in-from-top-[22%] sm:max-w-sm sm:rounded-lg",
+        "fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-white p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-top-[22%] data-[state=open]:slide-in-from-top-[22%] sm:max-w-sm sm:rounded-lg",
         className
       )}
       {...props}
@@ -123,12 +123,15 @@ interface ModalProps {
 const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children, title }) => {
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
-      <DialogContent>
-        <DialogHeader>
-          {title && <DialogTitle>{title}</DialogTitle>}
-        </DialogHeader>
-        {children}
-      </DialogContent>
+      <DialogPortal>
+        <DialogOverlay />
+        <DialogContent>
+          <DialogHeader>
+            {title && <DialogTitle>{title}</DialogTitle>}
+          </DialogHeader>
+          {children}
+        </DialogContent>
+      </DialogPortal>
     </Dialog>
   );
 };
