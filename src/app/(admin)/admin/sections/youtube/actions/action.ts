84ebbd3ad2653c03ddcb1802ce85baa598d34558
NikKowPHP <nik.kow@outlook.com>
2025-03-18 09:08:58 +0100

    create update action for youtubve

---
+++ src/app/(admin)/admin/sections/youtube/actions/action.ts
@@ -0,0 +1,10 @@
+'use server'
+import { youtubeService } from '@/lib/services/youtube.service'
+import { YoutubeModel } from '@/domain/models/models'
+
+export async function updateYoutubeAction(data: Partial<YoutubeModel>) {
+  if (!data.youtube_url) {
+    throw new Error('Youtube URL is required')
+  }
+  return await youtubeService.updateYoutube(data.youtube_url)
+}
