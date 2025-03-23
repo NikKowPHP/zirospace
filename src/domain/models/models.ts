423ca8e426174e8c7fe340c1fb674d62fdf1f551
NikKowPHP <nik.kow@outlook.com>
2025-03-23 12:29:49 +0100

    get together all models

--- src/domain/models/models.ts
+++ src/domain/models/models.ts
@@ -3,3 +3,86 @@ export interface YoutubeModel {
     id: string;
     youtube_url: string;
 }
+
+
+export interface Image {
+  url: string;
+  alt: string;
+}
+
+export interface CaseStudy {
+  readonly id: string;
+  readonly slug: string;
+  readonly title: string;
+  readonly subtitle: string;
+  readonly description: string;
+  tags: readonly string[];
+  readonly images: readonly Image[];
+  readonly ctaUrl: string;
+  readonly createdAt: Date;
+  readonly updatedAt: Date;
+  readonly color: string;
+  readonly orderIndex: number;
+  readonly backgroundColor: string;
+  readonly theme: string;
+}
+
+export interface Banner {
+    id: string;
+    title: string;
+    content: string;
+    subtitle?: string;
+    imageUrl?: string;
+    youtubeUrl?: string;
+    startDate?: Date;
+    endDate?: Date;
+    isActive: boolean;
+    createdAt: Date;
+    updatedAt: Date;
+    ctaButtonText?: string;
+    ctaButtonLink?: string;
+  }
+
+
+  export interface BlogPost {
+    id: string;
+    slug: string;
+    title: string;
+    imageurl: string;
+    createdAt: string;
+    imageAlt?: string;
+    excerpt: string;
+    contentHtml: string;
+    isPinned: boolean;
+  }
+
+  export interface Testimonial {
+    readonly id: string;
+    readonly author: string;
+    readonly role: string;
+    readonly company: string;
+    readonly quote: string;
+    readonly image: string;
+    readonly image_alt: string;
+    readonly createdAt: Date;
+    readonly updatedAt: Date;
+  }
+
+
+
+
+
+export interface CaseStudyImage {
+    id: string;
+    image: string;
+    alt: string;
+    createdAt?: Date;
+    updatedAt?: Date;
+  }
+  
+  export interface CaseStudySlider {
+    readonly id: string;
+    images: CaseStudyImage[];
+    createdAt?: Date;
+    updatedAt?: Date;
+  }
