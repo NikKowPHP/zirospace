a79c42a9b16fe4694c361fa0cd4a841bfc4512b5
NikKowPHP <nik.kow@outlook.com>
2025-03-29 18:06:18 +0100

    add image, modify the header size

--- src/components/sections/our-services/our-services.tsx
+++ src/components/sections/our-services/our-services.tsx
@@ -1,6 +1,10 @@
 import { Suspense } from 'react'
 import { getTranslations } from 'next-intl/server'
-import { getServiceItems,  ServiceItem as ServiceItemType } from '@/lib/data/our-services'
+import {
+  getServiceItems,
+  ServiceItem as ServiceItemType,
+} from '@/lib/data/our-services'
+import Image from 'next/image'
 export const OurServices = async () => {
   const t = await getTranslations('servicesSection')
   const serviceItems = getServiceItems()
@@ -14,7 +18,7 @@ export const OurServices = async () => {
       }
     >
       <section
-      id='our-services'
+        id="our-services"
         className=" py-[100px] "
         itemScope
         itemType="https://schema.org/WebPageElement"
@@ -27,17 +31,16 @@ export const OurServices = async () => {
 }
 const ServiceTitleSubtitle = ({ t }: { t: any }) => {
   return (
-    <div className='mb-[36px] flex flex-col items-center justify-center '>
+    <div className="mb-[36px] flex flex-col items-center justify-center ">
       <h1
         className="text-[40px] sm:text-[56px] lg:text-[48px] leading-[1.1] font-medium tracking-[-0.02em] text-primary mb-[16px]"
         itemProp="headline"
       >
         {t('ourServicesTitle')}
-       
       </h1>
       <h3 className="text-[14px] sm:text-[16px] lg:text-[18px] text-gray-600 leading-relaxed max-w-xl sm:max-w-2xl px-4 sm:px-0">
-          {t('ourServicesSubtitle')}
-        </h3>
+        {t('ourServicesSubtitle')}
+      </h3>
     </div>
   )
 }
@@ -59,15 +62,33 @@ const ServiceItemList = async ({
   )
 }
 
-const ServiceItem = ({ t, index, item }: { t: any; index: number; item: any }) => {
+const ServiceItem = ({
+  t,
+  index,
+  item,
+}: {
+  t: any
+  index: number
+  item: any
+}) => {
   return (
-    <div key={index} className="p-[36px] rounded-xl  bg-gray-100 shadow-sm flex flex-col gap-[16px] ">
-      <h4 className='text-[18px] leading-[1.2]'>{t(`${item.subtitle}`)}</h4>
-      <h2 className="text-[48px] leading-[1.2] text-black">{t(`${item.title}`)}</h2>
-      <p className=" text-[18px] text-gray-600">
-        {t(`${item.description}`)}
-
-      </p>
+    <div
+      key={index}
+      className="p-[30px] rounded-xl  bg-gray-100 shadow-sm flex flex-col gap-[16px] "
+    >
+      <div className="w-full h-full justify-center flex max-h-52 relative">
+        <Image
+          src={'/images/client1.webp'}
+          alt="image"
+          className="object-cover rounded-lg"
+          width={300}
+          height={200}
+        />
+      </div>
+      <h2 className="text-[30px] leading-[1.2] text-black">
+        {t(`${item.title}`)}
+      </h2>
+      <p className=" text-[18px] text-gray-600">{t(`${item.description}`)}</p>
     </div>
   )
 }
