83d0b89aa1807b0cf76d8f7bc676867800c08b06
NikKowPHP <nik.kow@outlook.com>
2025-03-30 07:53:39 +0200

    modify hero service

--- src/lib/services/heroService.ts
+++ src/lib/services/heroService.ts
@@ -1,30 +1,31 @@
-import { HeroModel } from "@/domain/models/models"
-import { heroRepository } from "@/lib/repositories/hero.repository"
-import { logger } from "@/lib/logger
+import { HeroModel } from '@/domain/models/models'
+import { heroRepository } from '@/lib/repositories/hero.repository'
+import { logger } from '@/lib/logger'
 
-export class HeroService{
-  private heroRepository: IHeroRepository 
+export class HeroService {
+  private heroRepository: IHeroRepository
   constructor() {
     // if(process.env.MOCK_REPOSITORIES === 'true') {
     //   this.bannerRepository = bannerRepositoryLocal
     // } else {
-      this.heroRepository = heroRepository
+    this.heroRepository = heroRepository
     // }
   }
 
   getHeroSection = async (locale: string): Promise<HeroModel[]> => {
     const heroSection = await this.heroRepository.getHeroSection(locale)
-    logger.log('heroSection', heroSection);
-    return heroSection;
+    logger.log('heroSection', heroSection)
+    return heroSection
   }
 
-  updateBanner = async (id: string, heroData: Partial<HeroModel>, locale: string): Promise<HeroModel | null> => {
-
+  updateBanner = async (
+    id: string,
+    heroData: Partial<HeroModel>,
+    locale: string
+  ): Promise<HeroModel | null> => {
     return this.heroRepository.updateHeroSection(id, heroData, locale)
   }
-
 }
 
 // export singleton
 export const heroService = new HeroService()
-
