0787d8f32c405c16fa1d1bddc4ccc55724c496e1
NikKowPHP <nik.kow@outlook.com>
2025-04-05 17:33:44 +0200

    fix hero

--- src/components/sections/hero/hero.tsx
+++ src/components/sections/hero/hero.tsx
@@ -6,7 +6,8 @@ export const HeroSection = async ({ locale }: { locale: string }) => {
 
   return (
     <section
-      className="w-screen max-w-[100vw] -mx-4 sm:-mx-6 lg:-mx-8 flex flex-col justify-center items-center pt-[180px] sm:pt-[175px] sm:pb-[100px] space-y-6 sm:space-y-8 min-h-screen"
+      className="md:w-screen md:max-w-[100vw]  sm:-mx-6 lg:-mx-8 flex flex-col justify-center items-center pt-[100px] sm:pt-[175px] sm:pb-[100px] space-y-6 sm:space-y-8 sm:min-h-screen px-4 sm:px-6"
+
       itemScope
       itemType="https://schema.org/WebPageElement"
       style={{
@@ -20,7 +21,7 @@ export const HeroSection = async ({ locale }: { locale: string }) => {
     >
       {/* Optimize heading for LCP */}
       <h1
-        className=" text-[64px] sm:text-[72px] lg:text-[64px] leading-[1.1] font-medium tracking-[-0.02em] mb-2 text-center max-w-[650px] mx-auto"
+        className="text-[64px] sm:text-[72px] lg:text-[64px] leading-[1.1] font-medium tracking-[-0.02em] mb-2 text-center max-w-[90vw] sm:max-w-[650px] mx-auto"
         itemProp="headline"
         style={{
           contentVisibility: 'auto',
@@ -32,7 +33,7 @@ export const HeroSection = async ({ locale }: { locale: string }) => {
 
       {/* Optimize subtitle */}
       <p
-        className="text-[18px] sm:text-[20px] lg:text-[24px] text-gray-600 leading-relaxed px-4 sm:px-0 max-w-[800px] mx-auto text-center"
+        className="text-[16px] sm:text-[20px] lg:text-[24px] text-gray-600 leading-relaxed px-2 sm:px-0 max-w-[90vw] sm:max-w-[800px] mx-auto text-center"
         style={{
           contentVisibility: 'auto',
           containIntrinsicSize: '24px',
