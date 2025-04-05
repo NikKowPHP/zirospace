a68b2e4ed2c61b78e79336ff4ab2233575035e3d
NikKowPHP <nik.kow@outlook.com>
2025-04-05 13:55:52 +0200

    update types to match the prisma

--- src/domain/models/models.ts
+++ src/domain/models/models.ts
@@ -88,16 +88,17 @@ export interface Banner {
 
 
 export interface CaseStudyImage {
-    id: string;
-    image: string;
-    alt: string;
-    createdAt?: Date;
-    updatedAt?: Date;
-  }
-  
-  export interface CaseStudySlider {
-    readonly id: string;
-    images: CaseStudyImage[];
-    createdAt?: Date;
-    updatedAt?: Date;
-  }
+  id: string
+  image: string
+  alt: string
+  createdAt: Date
+  updatedAt: Date
+  sliderId?: string
+}
+
+export interface CaseStudySlider {
+  id: string
+  images: CaseStudyImage[]
+  createdAt: Date
+  updatedAt: Date
+}
