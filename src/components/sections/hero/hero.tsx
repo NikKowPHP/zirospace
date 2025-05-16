c5aed670fe4cf443e6af3825fd096e0674c11d1e
NikKowPHP <nik.kow@outlook.com>
2025-05-16 08:10:05 +0200

    fix: adjust margin-top for hero section for better layout

--- src/components/sections/hero/hero.tsx
+++ src/components/sections/hero/hero.tsx
@@ -6,7 +6,7 @@ export const HeroSection = async ({ locale }: { locale: string }) => {
 
   return (
     <section
-      className="md:w-screen md:max-w-[100vw]  sm:-mx-6 lg:-mx-8 flex flex-col justify-center items-center   space-y-6 sm:space-y-8 min-h-screen px-4 sm:px-6"
+      className="md:w-screen md:max-w-[100vw] -mt-[75px]  sm:-mx-6 lg:-mx-8 flex flex-col justify-center items-center   space-y-6 sm:space-y-8 min-h-screen px-4 sm:px-6"
 
       itemScope
       itemType="https://schema.org/WebPageElement"
